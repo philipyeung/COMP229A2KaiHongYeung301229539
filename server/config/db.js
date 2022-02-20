2d2aa15d0b40e52d91629ce970698ec641d7c9b0
philipyeung <philipyeung88@hotmail.com>
2022-02-20 16:42:09 +0800

    update db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,25 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1:27017",
+    
     //"URI": "mongodb://localhost/login",
+    //"URI": "mongodb://127.0.0.1:27017",
+    "URI": "mongodb+srv://admin:7IFGRZMfUNXbPhEi@cluster0.37bet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
+
+
+    ////mongodb atlas
+    //admin
+    //7IFGRZMfUNXbPhEi
+
+
+/*    
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:<password>@cluster0.37bet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
 }
